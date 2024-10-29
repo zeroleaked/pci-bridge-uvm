ed3e011d6916c2d1afd5e8e893aee2ec85aa67a8
zeroleaked <zirlysukarno@gmail.com>
2024-10-29 14:07:20 +0700

    add vseq

--- tests/sequence_lib/src/wb_pci_rw_seq.sv
+++ tests/sequence_lib/src/wb_pci_rw_seq.sv
@@ -19,11 +19,19 @@ class wb_pci_rw_seq extends uvm_sequence#(wb_transaction);
 	//////////////////////////////////////////////////////////////////////////////
 	virtual task body();
 		wb_write_seq write_seq;
+		wb_read_seq read_seq;
+
 		write_seq = wb_write_seq::type_id::create("req");
 		write_seq.set_address(32'h3);
 		write_seq.set_data(32'h12153524);
 		write_seq.start(m_sequencer);
     	`uvm_info(get_type_name(), "normal single memory write through wb image to pci sequence completed", UVM_LOW)
+
+		read_seq = wb_read_seq::type_id::create("req");
+		read_seq.set_address(32'h3);
+		read_seq.set_data(32'h12153524);
+		read_seq.start(m_sequencer);
+    	`uvm_info(get_type_name(), "normal single memory read through wb image to pci sequence completed", UVM_LOW)
 	endtask
 
 endclass
